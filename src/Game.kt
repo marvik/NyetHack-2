8d6c346ccc2a041ad4ec10f37e97dabb70b2b543
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2019-04-28 21:41:35 +0900

    do challenge

--- src/Game.kt
+++ src/Game.kt
@@ -1,3 +1,6 @@
+import kotlin.math.min
+import kotlin.math.max
+
 fun main(args: Array<String>) {
     val name = "Madrigal"
     val healthPoints = 89
@@ -11,6 +14,15 @@ fun main(args: Array<String>) {
 
     // Player status
     printPlayerStatus(auraColor, isBlessed, name, healthStatus)
+
+    val inebriation = castFireball()
+    when(inebriation) {
+        in 1..10 -> println("tipsy")
+        in 11..20 -> println("sloshed")
+        in 21..30 -> println("soused")
+        in 31..40 -> println("stewed")
+        in 41..50 -> println("..t0aSt3d")
+    }
 }
 
 private fun printPlayerStatus(
@@ -23,14 +35,11 @@ private fun printPlayerStatus(
     println("$name $healthStatus")
 }
 
-private fun auraColor(isBlessed: Boolean, healthPoints: Int, isImmortal: Boolean): String {
-    val auraVisible = isBlessed && healthPoints > 50 || isImmortal
-    val auraColor = if (auraVisible) "GREEN" else "NONE"
-    return auraColor
-}
+private fun auraColor(isBlessed: Boolean, healthPoints: Int, isImmortal: Boolean): String =
+    if (isBlessed && healthPoints > 50 || isImmortal) "GREEN" else "NONE"
 
-private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean): String {
-    val healthStatus = when (healthPoints) {
+private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean): String =
+    when (healthPoints) {
         100 -> "is in excellent condition!"
         in 90..99 -> "has a few scratches."
         in 75..89 -> if (isBlessed) {
@@ -41,5 +50,8 @@ private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean): String {
         in 15..74 -> "looks pretty hurt."
         else -> "is in awful condition!"
     }
-    return healthStatus
+
+private fun castFireball(numFireballs: Int = 2): Int {
+    println("A glass of Fireball springs into existence. (x$numFireballs)")
+    return max(min(numFireballs, 50), 1)
 }
