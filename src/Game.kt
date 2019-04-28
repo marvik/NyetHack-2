6a60531b70b4503f117ea6b0857aacdc0470195a
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2019-04-28 16:20:52 +0900

    add when

--- src/Game.kt
+++ src/Game.kt
@@ -10,20 +10,16 @@ fun main(args: Array<String>) {
     val auraColor = if (auraVisible) "GREEN" else "NONE"
     println(auraColor)
 
-    val healthStatus = if (healthPoints == 100) {
-        "is in excellent condition!"
-    } else if (healthPoints in 90..99) {
-        "has a few scratches."
-    } else if (healthPoints in 75..89) {
-        if (isBlessed) {
-            "has some minor wounds but is healing quite quickly!"
+    val healthStatus = when (healthPoints) {
+        100 -> "is in excellent condition!"
+        in 90..99 -> "has a few scratches."
+        in 75..89 -> if (isBlessed) {
+            "has some minor wounds but is healing quite quickley!"
         } else {
             "has some minor wounds."
         }
-    } else if (healthPoints in 15..74) {
-        "looks pretty hurt."
-    } else {
-        "is in awful condition!"
+        in 15..74 -> "looks pretty hurt."
+        else -> "is in awful condition!"
     }
 
     println("$name $healthStatus")
