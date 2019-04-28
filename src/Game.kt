87448949be487960eebbd971d3f1694484bafdf6
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2019-04-28 13:59:00 +0900

    change condition-expression

--- src/Game.kt
+++ src/Game.kt
@@ -1,31 +1,30 @@
 fun main(args: Array<String>) {
     val name = "Madrigal"
-    var healthPoints = 89
-    var isBlessed = true
+    val healthPoints = 89
+    val isBlessed = true
 
     val isImmortal = false
 
     // Aura
     val auraVisible = isBlessed && healthPoints > 50 || isImmortal
-    if (auraVisible) {
-        println("GREEN")
-    } else {
-        println("NONE")
-    }
+    val auraColor = if (auraVisible) "GREEN" else "NONE"
+    println(auraColor)
 
-    if (healthPoints == 100) {
-        println("$name is in excellent condition!")
+    val healthStatus = if (healthPoints == 100) {
+        "is in excellent condition!"
     } else if (healthPoints >= 90) {
-        println("$name has a few scratches.")
+        "has a few scratches."
     } else if (healthPoints >= 75) {
         if (isBlessed) {
-            println("$name has some minor wounds but is healing quite quickly!")
+            "has some minor wounds but is healing quite quickly!"
         } else {
-            println("$name has some minor wounds.")
+            "has some minor wounds."
         }
     } else if (healthPoints >= 15) {
-        println("$name looks pretty hurt.")
+        "looks pretty hurt."
     } else {
-        println("$name is in awful condition!")
+        "is in awful condition!"
     }
+
+    println("$name $healthStatus")
 }
