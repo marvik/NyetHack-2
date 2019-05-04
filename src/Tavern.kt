f96d9d181e0a73ab680f460d244d14fa909c5ba6
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2019-05-04 22:56:24 +0900

    fix typo

--- src/Tavern.kt
+++ src/Tavern.kt
@@ -10,7 +10,7 @@ val uniquePatrons = mutableSetOf<String>()
 val menuList = File("data/tavern-menu-data.txt")
     .readText()
     .split("\n")
-val patronGold = mapOf("Eli" to 10.5, "Mardoc" to 8.0, "Sophie" to 5.5)
+val patronGold = mapOf("Eli" to 10.5, "Mordoc" to 8.0, "Sophie" to 5.5)
 
 fun main(args: Array<String>) {
     if (patronList.contains("Eli")) {
@@ -44,6 +44,9 @@ fun main(args: Array<String>) {
     }
 
     println(patronGold)
+    println(patronGold["Eli"])
+    println(patronGold["Mordoc"])
+    println(patronGold["Sophie"])
 }
 
 fun performPurchase(price: Double) {
