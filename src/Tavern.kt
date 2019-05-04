aeb90cc92310b4be7563c30d8deae930e2289dc5
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2019-05-04 19:51:53 +0900

    add menu data

--- src/Tavern.kt
+++ src/Tavern.kt
@@ -1,8 +1,13 @@
+import java.io.File
+
 const val TAVERN_NAME = "Taernyl's Folly"
 
 var playerGold = 10
 var playerSilver = 10
 val patronList = mutableListOf("Eli", "Mordoc", "Sophie")
+val menuList = File("data/tavern-menu-data.txt")
+    .readText()
+    .split("\n")
 
 fun main(args: Array<String>) {
     if (patronList.contains("Eli")) {
@@ -19,8 +24,13 @@ fun main(args: Array<String>) {
 
     patronList.forEachIndexed { index, patron ->
         println("Good evening, $patron - you're #${index + 1} in line.")
-        placeOrder(patron, "shandy,Dragon's Breath,5.91")
+        placeOrder(patron, menuList.shuffled().first())
     }
+
+    menuList.forEachIndexed { index, data ->
+        println("$index, $data")
+    }
+
 }
 
 fun performPurchase(price: Double) {
