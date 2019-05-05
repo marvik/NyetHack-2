bafb3631ed07764fdfd737e1652ee18d830fcc96
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2019-05-06 00:20:20 +0900

    fix errors

--- src/Game.kt
+++ src/Game.kt
@@ -2,17 +2,12 @@ fun main(args: Array<String>) {
     val player = Player()
     player.castFireball()
 
-    // Aura
-    val auraColor = player.auraColor()
-
-    val healthStatus = player.formatHealthStatus()
-
     // Player status
     printPlayerStatus(player)
 }
 
 private fun printPlayerStatus(player: Player) {
     println("(Aura: ${player.auraColor()}) (Blessed: ${if (player.isBlessed) "YES" else "NO"})")
-    println("${player.name} ${player.healthStatus()}")
+    println("${player.name} ${player.formatHealthStatus()}")
 }
 
