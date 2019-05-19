48f088ee6b72035eb7b806ff8086c43832d48dd6
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2019-05-19 22:58:24 +0900

    add loot converter

--- src/com/bignerdranch/nyethack/Generics.kt
+++ src/com/bignerdranch/nyethack/Generics.kt
@@ -1,7 +1,16 @@
 package com.bignerdranch.nyethack
 
 class LootBox<T>(item: T) {
+    var open = false
     private var loot: T = item
+
+    fun fetch(): T? {
+        return loot.takeIf { open }
+    }
+
+    fun <R> fetch(lootModFunction: (T) -> R): R? {
+        return lootModFunction(loot).takeIf { open }
+    }
 }
 
 class Fedora(val name: String, val value: Int)
@@ -11,4 +20,14 @@ class Coin(val value: Int)
 fun main(args: Array<String>) {
     val lootBoxOne: LootBox<Fedora> = LootBox(Fedora("a generic-looking fedora", 15))
     val lootBoxTwo: LootBox<Coin> = LootBox(Coin(15))
+
+    lootBoxOne.open = true
+    lootBoxOne.fetch()?.run {
+        println("You retrieve $name from the box!")
+    }
+
+    val coin = lootBoxOne.fetch() {
+        Coin(it.value * 3)
+    }
+    coin?.let { println(it.value) }
 }
